0f4e72db4f5fc8af59e3cf2b915bc961124e040b
0w0hu <98564080+0w0hu@users.noreply.github.com>
2026-05-01 12:23:08 -0700

    Add: combinations-of-a-phone-number - submission-0

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-0.py
@@ -0,0 +1,27 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        res = []
+        cur = ""
+        digitmap = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "qprs",
+            "8": "tuv",
+            "9": "wxyz",
+        }
+
+        def helper(i, cur):
+            if len(cur) == len(digits):
+                res.append(cur)
+                return
+            
+            for c in digitmap[digits[i]]:
+                
+                helper(i+1, cur+c)
+
+        if digits:
+            helper(0, cur)
+        return res
