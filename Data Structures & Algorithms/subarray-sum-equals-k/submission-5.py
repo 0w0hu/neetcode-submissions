2e2abe51bcfa97e0de00d64c9d7ad08e84458253
0w0hu <98564080+0w0hu@users.noreply.github.com>
2026-07-07 18:25:55 -0700

    Add: subarray-sum-equals-k - submission-5

---
+++ Data Structures & Algorithms/subarray-sum-equals-k/submission-5.py
@@ -0,0 +1,11 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        hashmap = {0:1}
+        cursum = 0
+        res = 0
+        for n in nums:
+            cursum += n
+            diff = cursum - k
+            res += hashmap.get(diff, 0)
+            hashmap[cursum] = 1 + hashmap.get(cursum, 0)
+        return res
